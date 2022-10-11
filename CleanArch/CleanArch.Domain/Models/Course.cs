232b943fcaaa4b3f9ba50a3f6d0d47871cf51837
Abakumov Valeriy <6594815+abakumov-v@users.noreply.github.com>
2022-10-11 17:57:40 +0300

    String properties of the "Course" class were made as nullable

--- CleanArch/CleanArch.Domain/Models/Course.cs
+++ CleanArch/CleanArch.Domain/Models/Course.cs
@@ -9,8 +9,8 @@ namespace CleanArch.Domain.Models
     public class Course
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string ImageUrl { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
     }
 }
