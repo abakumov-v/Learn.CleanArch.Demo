bf733eb0a8a3b2d89beb0f5a954aae5fee3e7c6e
Abakumov Valeriy <6594815+abakumov-v@users.noreply.github.com>
2022-10-12 15:58:56 +0300

    added the correct MVC (Model-View-Controller) project besides current MVC (which is not MVC but just RazorPages) project

---
+++ CleanArch/CleanArch.UI.Mvc.Web/Controllers/CourseController.cs
@@ -0,0 +1,21 @@
+﻿using CleanArch.Application.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.UI.Mvc.Web.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _courseService.GetCourses();
+            return View(model);
+        }
+    }
+}
---
+++ CleanArch/CleanArch.UI.Mvc.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CleanArch.UI.Mvc.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
