ceb9f4cee63b416059b2ad9b2afeb9d461cafc78
Abakumov Valeriy <6594815+abakumov-v@users.noreply.github.com>
2022-10-19 18:56:16 +0300

    configured the Swagger

--- CleanArch/CleanArch.Api/Controllers/CourseController.cs
+++ CleanArch/CleanArch.Api/Controllers/CourseController.cs
@@ -1,18 +1,28 @@
+using CleanArch.Application.Abstract;
+using CleanArch.Application.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArch.Api.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class CourseController : ControllerBase
     {
-        private readonly ILogger<CourseController> _logger;
+        private readonly ILogger _logger;
+        private readonly ICourseService _courseService;
 
-        public CourseController(ILogger<CourseController> logger)
+        public CourseController(ILogger<CourseController> logger, 
+            ICourseService courseService)
         {
             _logger = logger;
+            _courseService = courseService;
         }
 
-
+        [HttpPost]
+        public IActionResult Post([FromBody] CourseViewModel model)
+        {
+            _courseService.Create(model);
+            return Ok(model);
+        }
     }
 }
--- CleanArch/CleanArch.Api/Program.cs
+++ CleanArch/CleanArch.Api/Program.cs
@@ -1,6 +1,10 @@
 using CleanArch.Infra.Data.Context;
 using CleanArch.Infra.Ioc;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.Swagger;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,10 +17,17 @@ builder.Services.AddDbContext<UniversityDbContext>(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(e =>
+{
+    e.SwaggerDoc("v1", new OpenApiInfo { Title = "University Api", Version = "v1" });
+});
 
-// Register our custom dependencies
+#region Register our custom dependencies
+
 DependencyContainer.RegisterServices(builder.Services);
+builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
+
+#endregion
 
 var app = builder.Build();
 
@@ -24,7 +35,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "University API v1");
+    });
 }
 
 app.UseHttpsRedirection();
