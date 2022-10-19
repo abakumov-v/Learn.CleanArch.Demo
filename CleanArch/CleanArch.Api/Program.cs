c5cfcdf20f2c9a7c544d470d5f803ff0b09f7ad1
Abakumov Valeriy <6594815+abakumov-v@users.noreply.github.com>
2022-10-19 18:41:51 +0300

    configured Api project

--- CleanArch/CleanArch.Api/Program.cs
+++ CleanArch/CleanArch.Api/Program.cs
@@ -1,11 +1,22 @@
+using CleanArch.Infra.Data.Context;
+using CleanArch.Infra.Ioc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<UniversityDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("UniversityDBConnection"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register our custom dependencies
+DependencyContainer.RegisterServices(builder.Services);
 
 var app = builder.Build();
 
