a67323e3f3ebc9ebf7ef7204b440b6e713d52ac8
Abakumov Valeriy <6594815+abakumov-v@users.noreply.github.com>
2022-10-20 11:52:13 +0300

    added automaper configuration

---
+++ CleanArch/CleanArch.Application/Mappings/AutomapperConfig.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Mappings
+{
+    public class AutomapperConfig
+    {
+        public static MapperConfiguration RegisterMappings()
+        {
+            return new MapperConfiguration(config =>
+            {
+                config.AddProfile(new ViewModelToDomainProfile());
+                config.AddProfile(new DomainToViewModelProfile());
+            });
+        }
+    }
+}
